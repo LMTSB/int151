7aa5eb0409464931c826cfb58d7de47d34dd4841
pattui <pattyzul@users.noreply.github.com.>
2025-08-26 16:55:07 +0700

    week3

---
+++ note/week03.md
@@ -0,0 +1,12 @@
+*** JS arrays method ***
+-------
+- arrays method are higher order function
+- function without a name are called anonymous function there are use of anonymous function ex. one time use function 
+
+- Array methods 
+    - filter => filter array items using call back function which need boolean to filter
+    - map => create new array/object with results of calling provided function on every element in the calling array
+    - include => find array items that match the condition (condition = true)
+    - .some => "for some" means that at least one value in array match the conditions make the result to true
+    - .every => "for every" means that just 1 value that doesn't match the conditions could lead result to false
+    - reduce => merge value of number in array  
---
+++ week3.js
@@ -0,0 +1,52 @@
+function p(...t) {console.log(...t)};
+const words = ['apple','banana','sugar apple','grape fruit'];
+const filter = words.filter( word => word.length > 6); // anonymous function is "word" which indicate word that have length > 6 
+
+const map1 = words.map( word => word.toUpperCase()); // example of map method
+
+const students = [
+  { id: 1, name: "Somchai Jaidee" },
+  { id: 2, name: "Suda Deejai" },
+  { id: 3, name: "John Smith" },
+]
+const pick = students.map(student => student.id).filter(id => id % 2 === 0); // example of map method with object
+
+p(pick);
+p(map1);
+p(filter);
+
+const mangos = ['Mango','Apple','mangosteen','orange','mangoes'];
+
+const choose = mangos.filter(mango => mango.toLowerCase().includes('mango'))
+p(choose);
+
+const words2 = ['Apple','sugar apple','mangosteen','green apple','banana'];
+
+const choose2 = words2.filter(apple => apple.toLowerCase().includes('apple')? "have apple":"don't contain apple");
+choose2;
+
+const cart = [
+    {productId: 1001, price: 500, quantity: 2},
+    {productId: 1002, price: 10, quantity: 3},
+    {productId: 1003, price: 5, quantity: 10},
+]
+// find total price of the cart
+const totalPrice = cart.reduce((sum, product) => sum + product.price * product.quantity ,0) // (sum,product)
+p(`totalPrice: ${totalPrice}`);
+
+
+// cart[cart.length] = { productId: 5555, price: 1, quantity: 4 }
+//cart.unshift({ productId: 5555, price: 1, quantity: 4 })
+//console.log(cart)
+//p(cart.length);
+//p(cart.shift());
+//p(cart.length);
+//p(cart.unshift({ productId: 5555, price: 1, quantity: 4 }));
+
+const words3 = ["mango","apple","mangosteen","orange","mangoes"]
+words3.splice(2 ,1 ,"banana");
+p(words3);
+words3.splice(3 ,2);
+p(words3);
+words3.splice(2 ,1 ,"melon" ,"cherry");
+p(words3);
